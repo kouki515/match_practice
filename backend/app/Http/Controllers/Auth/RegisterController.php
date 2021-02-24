bf07122a34991ee9baef864638f33d4b0c9abd50
kouki515 <71614240+kouki515@users.noreply.github.com>
2021-02-25 02:09:30 +0900

    RegisterControllerの処理の切り分け

--- backend/app/Http/Controllers/Auth/RegisterController.php
+++ backend/app/Http/Controllers/Auth/RegisterController.php
@@ -9,8 +9,9 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
-// 追記
 use Intervention\Image\Facades\Image;
+use App\Services\CheckExtensionServices;
+use App\Services\FileUploadServices;
 
 class RegisterController extends Controller
 {
@@ -69,45 +70,16 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        //引数 $data から name='img_name'を取得(アップロードするファイル情報)
         $imageFile = $data['img_name'];
 
-        //$imageFileからファイル名を取得(拡張子あり)
-        $filenameWithExt = $imageFile->getClientOriginalName();
+        $list = FileUploadServices::fileUpload($imageFile);
 
-        //拡張子を除いたファイル名を取得
-        $fileName = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+        list($extension, $fileNameToStore, $fileData) = $list;
 
-        //拡張子を取得
-        $extension = $imageFile->getClientOriginalExtension();
+        $data_url = CheckExtensionServices::checkExtension($fileData, $extension);
 
-        // ファイル名_時間_拡張子 として設定
-        $fileNameToStore = $fileName . '_' . time() . '.' . $extension;
-
-        //画像ファイル取得
-        $fileData = file_get_contents($imageFile->getRealPath());
-
-        //拡張子ごとに base64エンコード実施
-        if ($extension = 'jpg') {
-            $data_url = 'data:image/jpg;base64,' . base64_encode($fileData);
-        }
-
-        if ($extension = 'jpeg') {
-            $data_url = 'data:image/jpg;base64,' . base64_encode($fileData);
-        }
-
-        if ($extension = 'png') {
-            $data_url = 'data:image/png;base64,' . base64_encode($fileData);
-        }
-
-        if ($extension = 'gif') {
-            $data_url = 'data:image/gif;base64,' . base64_encode($fileData);
-        }
-
-        //画像アップロード(Imageクラス makeメソッドを使用)
         $image = Image::make($data_url);
 
-        //画像を横400px, 縦400pxにリサイズし保存
         $image->resize(400, 400)->save(storage_path() . '/app/public/images/' . $fileNameToStore);
 
         return User::create([
