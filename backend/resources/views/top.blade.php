4796d691a59ee0a6fa43730b9e2a28a54318063b
kouki515 <71614240+kouki515@users.noreply.github.com>
2021-02-27 18:20:13 +0900

    jTinderの実装

--- backend/resources/views/top.blade.php
+++ backend/resources/views/top.blade.php
@@ -4,8 +4,9 @@
 <div class="loginPage">
   <div class="container">
     <div class="loginPage_contents">
-    <h1 class="h3 loginPage_contents_title">新しいマッチを見つけよう</h1>
-    <div class="btn loginPage_contents_btn"><a class="text-white" href="{{ route('login') }}">メールアドレスでログインする</a></div>
+      <h1 class="h3 loginPage_contents_title">新しいマッチを見つけよう</h1>
+      <div class="btn loginPage_contents_btn"><a class="text-white" href="{{ route('login') }}">メールアドレスでログインする</a>
+      </div>
     </div>
   </div>
 </div>
